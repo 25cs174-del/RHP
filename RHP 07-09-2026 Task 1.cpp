e4bd07e80720a3d484c210e54f43b37b0d5e36a2
25cs174-del <25cs174@rmkec.ac.in>
2026-09-07 10:00:05 +0530

    Add files via upload

---
+++ RHP 07-09-2026 Task 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+#define ll long long int
+int hcf(int a, int b) {
+    return b == 0 ? a : hcf(b, a % b);
+}
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << hcf(arr[0], arr[n - 1]) << endl;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
